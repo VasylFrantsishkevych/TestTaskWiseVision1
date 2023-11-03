2b40f7298dc36976d2f4d3fd59a6e8e2a73b17ac
VF <vasul.fr@gmail.com>
2023-11-03 12:46:05 +0200

    task1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatExpansionModule} from "@angular/material/expansion";
 import {MatPaginatorModule} from "@angular/material/paginator";
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/components/search-repositories/search-repositories.component.ts
+++ src/app/components/search-repositories/search-repositories.component.ts
@@ -1,25 +1,39 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+
 import {SearchService} from "../../services";
 import {IItem} from "../../interfaces";
+import {ActivatedRoute, Router, RouterStateSnapshot} from "@angular/router";
+import {MatPaginator} from "@angular/material/paginator";
+
 
 @Component({
   selector: 'app-search-repositories',
   templateUrl: './search-repositories.component.html',
   styleUrls: ['./search-repositories.component.css']
 })
-export class SearchRepositoriesComponent implements OnInit {
+export class SearchRepositoriesComponent implements OnInit, AfterViewInit {
   form: FormGroup;
   searchText: string;
+  // page: number;
   totalCount: number;
   repositories: IItem[];
 
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator
 
-  constructor(private searchService:SearchService) {
+
+  constructor(private searchService: SearchService, private router:Router) {
     this.initForm()
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.paginator.page.subscribe((page) => {
+      this.router.navigate([], {queryParams: {page: page.pageIndex+1}})
+    })
   }
 
   initForm(): void {
@@ -30,11 +44,14 @@ export class SearchRepositoriesComponent implements OnInit {
 
   search(): void {
     this.searchText = this.form.value.search;
-      this.searchService.getSearchRepositories(this.searchText).subscribe(value => {
+
+    this.searchService.getSearchRepositories(this.searchText).subscribe(value => {
       console.log(value);
       this.repositories = value.items;
       this.totalCount = value.total_count;
     });
     this.form.reset();
   }
+
+
 }
